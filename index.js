6b35ed1b0a050f011e9bb431538b3ab0fa95dd69
arthurfrank10 <142546310+arthurfrank10@users.noreply.github.com>
2023-08-21 21:43:59 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+* Arthur Frank
+* Estou gostando do curso
+*/
+console.log("ola mundo");
+//prompt("Digite seu nome");
+
+//variaveis
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log(variavelB);
+
+let numero = 50;
+console.log(numero);
+numero = 80;
+console.log(numero);
+
+const nome = "Arthur";
+const idade = 16;
+
+console.log("ola", "meu nome é", nome, "e eu tenho", idade, "anos.");
+
+const primeiro = "Arthur";
+const sobrenome = "Frank";
+const idadeExercicio = 16;
+const estudante = "sou estudante";
+
+console.log("ola,", "meu nome é", primeiro, sobrenome, ",", "tenho", idadeExercicio, "anos e eu", estudante, ".");
+
+const sarah = "adoro a sarinha";
+
+console.log("salve, meu nome é", primeiro, "e eu", sarah);
+
+const amar = "eu amo minha familia e meus amigos"
+
+console.log("meu nome é", primeiro, "e", amar);
